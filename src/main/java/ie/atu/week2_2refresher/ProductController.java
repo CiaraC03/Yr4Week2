981d9485c6d0a9372dced023c1d4fc482d5cf415
G00402357@atu.ie <G00402357@atu.ie>
2024-09-23 21:35:18 +0100

    added dependency injection

--- src/main/java/ie/atu/week2_2refresher/ProductController.java
+++ src/main/java/ie/atu/week2_2refresher/ProductController.java
@@ -1,5 +1,6 @@
 package ie.atu.week2_2refresher;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,19 +12,16 @@ import java.util.List;
 @RestController
 public class ProductController {
 
-    private List<Product> productList = new ArrayList<Product>();
+    private final ProductService productService;
 
-    @GetMapping("/products")
-    public List<Product> getProductList()
+    @Autowired
+    public ProductController(ProductService productService)
     {
-        return productList;
+        this.productService = productService;
     }
 
-    @PostMapping("/products")
-    public Product returnProducts(@RequestBody Product product)
-    {
-        productList.add(product);
-        return product;
+    public List<Product> retreiveProductList(){
+        return productService.getProductList();
     }
 
 }
