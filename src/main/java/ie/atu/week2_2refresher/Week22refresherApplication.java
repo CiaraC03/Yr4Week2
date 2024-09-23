0036abee2b80950db840687f3df782620e1b3444
G00402357@atu.ie <G00402357@atu.ie>
2024-09-23 21:10:16 +0100

    Created project

---
+++ src/main/java/ie/atu/week2_2refresher/Week22refresherApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week2_2refresher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week22refresherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week22refresherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/week2_2refresher/Week22refresherApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week2_2refresher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week22refresherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
